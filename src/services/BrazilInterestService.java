54ab5c979bbe31f459e625ed3439283e7f1b5e35
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-05-25 15:58:05 -0300

    Criado class juros Brasil

---
+++ src/services/BrazilInterestService.java
@@ -0,0 +1,14 @@
+package services;
+
+public class BrazilInterestService {
+
+	private double interestRate;
+
+	public BrazilInterestService(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	public double getInterestRate() {
+		return interestRate;
+	}
+}
