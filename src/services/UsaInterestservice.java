a31d8f2de7b054cd2834a510621a6764d7628950
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-05-25 15:55:19 -0300

    Criado class juros USA

---
+++ src/services/UsaInterestservice.java
@@ -0,0 +1,14 @@
+package services;
+
+public class UsaInterestservice {
+
+	private double interestRate;
+
+	public UsaInterestservice(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	public double getInterestRate() {
+		return interestRate;
+	}
+}
